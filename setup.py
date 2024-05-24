51448f8893e4e6cbc47ab0ab77cc1305890e1091
Koen <98043234+koen1711@users.noreply.github.com>
2024-05-24 12:51:03 +0200

    chore: change project name

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ README = (HERE / "README.md").read_text()
 
 # This call to setup() does all the work
 setup(
-    name="python-play",
+    name="corderius-play",
     version="1.0.0",
     description="The easiest way to make games and media projects in Python.",
     long_description=README,
