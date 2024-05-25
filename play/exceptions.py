1b1ddeba97884d045577d08df1f4c438db1a1e6a
Koen <98043234+koen1711@users.noreply.github.com>
2024-05-25 14:24:45 +0200

    chore: add pull_request.yaml workflow (#8)
    * chore: add pull_request.yaml workflow
    
    * chore: run black
    
    * chore: fix all pylint issues

--- play/exceptions.py
+++ play/exceptions.py
@@ -1,11 +1,12 @@
-import warnings
+"""Custom exceptions for the play library."""
 
 
 class Oops(Exception):
     def __init__(self, message):
         # for readability, always prepend exception messages in the library with two blank lines
-        message = '\n\n\tOops!\n\n\t'+message.replace('\n', '\n\t')+'\n'
-        super(Oops, self).__init__(message)
+        message = "\n\n\tOops!\n\n\t" + message.replace("\n", "\n\t") + "\n"
+        super(Oops, self).__init__(message)  # pylint: disable=super-with-arguments
+
 
 class Hmm(UserWarning):
     pass
