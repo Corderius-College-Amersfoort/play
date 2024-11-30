d9780aee5a509b613a8a793c90718942ef58a435
T <thollllllllllllllll@gmail.com>
2024-11-30 17:20:09 +0100

    Update physics.py
    Weird issue in checking it, worked fine however noted like this it should pass the test

--- tests/physics.py
+++ tests/physics.py
@@ -5,10 +5,10 @@ screeny = 0
 RADIUS = 100
 
 
-def test_physics(RADIUS):
+def test_physics(radius):
     import play
 
-    sprite = play.new_circle(color="gray", radius=RADIUS)
+    sprite = play.new_circle(color="gray", radius=radius)
     sprite.start_physics(obeys_gravity=True, bounciness=0, stable=True, friction=0)
 
     @play.when_program_starts
@@ -27,9 +27,9 @@ def test_physics(RADIUS):
     global y
     global screeny
     print(round(y))
-    print(round((screeny / 2 * -1) + RADIUS))
+    print(round((screeny / 2 * -1) + radius))
     # check if the y is within 1% of ((screeny /2 * -1) + size):
-    if round(y) != round((screeny / 2 * -1) + RADIUS):
+    if round(y) != round((screeny / 2 * -1) + radius):
         pytest.fail("The sprite should have fallen to the ground.")
 
 
